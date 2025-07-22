f82341e6d7e5c5692032f93b0a0015934d8f87e2
serkan <s8ersis@gmail.com>
2025-07-22 21:05:33 +0300

    feat: add middlewares to backend

---
+++ backend/src/middlewares/authentication.js
@@ -0,0 +1,17 @@
+"use strict"
+
+const jwt = require("../configs/requiredBasics").jwt
+
+module.exports = async (req, res, next) => {
+    const auth = req.headers?.authorization
+    const tokenKey = auth ? auth.split(" ") : null
+
+    if(tokenKey){
+        if(tokenKey[0] === "Bearer"){
+            jwt.verify(tokenKey[1], process.env.ACCESS_KEY, (error, accessData)=> {
+                req.user = accessData ? accessData : false
+            })
+        }
+    }
+    next()
+}
