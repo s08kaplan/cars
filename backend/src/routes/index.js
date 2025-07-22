5336b663e2a245a74f81f1305c569844c00a3ee2
serkan <s8ersis@gmail.com>
2025-07-22 21:08:39 +0300

    feat: add index route backend

---
+++ backend/src/routes/index.js
@@ -0,0 +1,13 @@
+"use strict"
+const router = require("../configs/requiredBasics").express.Router()
+
+router.use("/auth", require("./auth"))
+
+router.use("/users", require("./user"))
+
+router.use("/cars", require("./car"))
+
+router.use("/uploads", require("./upload"))
+
+
+module.exports = router
