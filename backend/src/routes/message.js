bece27364ed02f1701a05388693615bf307ef02e
serkan <s8ersis@gmail.com>
2025-07-27 11:58:39 +0300

    feat: add message part to database

---
+++ backend/src/routes/message.js
@@ -0,0 +1,23 @@
+"use strict";
+
+const router = require("express").Router();
+
+const message = require("../controllers/message");
+const isAdmin = require("../middlewares/authorized")
+router.use(isAdmin)
+
+router.route("/").get(message.list).post(message.create);
+router.route("/count").get(message.count);
+router.route("/unread").get(message.unRead);
+router.route("/recent").get(message.recent);
+
+
+router
+  .route("/:id")
+  .get(message.read)
+  .put(message.update)
+  .patch(message.update)
+  .delete(message.delete);
+
+/* ------------------------------------------------------- */
+module.exports = router;
