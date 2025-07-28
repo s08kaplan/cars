4c5253ca215bd0d47fbd3d4ca766ccb0be1e064b
serkan <s8ersis@gmail.com>
2025-07-28 22:58:11 +0300

    chore: update contact form with error messages

--- client/src/components/Error/MyError.tsx
+++ client/src/components/Error/MyError.tsx
@@ -1,12 +1,18 @@
-import React from 'react'
+type CustomError = {
+  message?: string;
+  customMessage?: string;
+  status?: number;
+};
 
-const MyError = () => {
-  return (
-    <section>
-        <h3>Ooooo something went wrong</h3>
-        <img src="/car-crash.avif" alt="car crash" className='min-w-full' />
+const MyError: React.FC<CustomError> = ({ message, customMessage }) => {
+    return (
+    <section className="text-center space-y-4">
+      <h3 className="text-xl font-bold text-red-600">
+        {message || customMessage || "Oops! Something went wrong."}
+      </h3>
+      <img src="/car-crash.avif" alt="car crash" className="w-full max-w-md mx-auto" />
     </section>
-  )
-}
+  );
+};
 
-export default MyError
+export default MyError;
