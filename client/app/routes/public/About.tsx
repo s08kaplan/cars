b06c41ba37817a187928bf0bb1d6f124ae2af476
serkan <s8ersis@gmail.com>
2025-11-02 23:24:47 +0300

    feat: add language to about page

--- client/app/routes/public/About.tsx
+++ client/app/routes/public/About.tsx
@@ -1,14 +1,28 @@
 import React from 'react'
 import SceneText from 'scenes/Scene-Text'
+import useLanguageStore from 'src/store/useLanguageStore';
 
 const About = () => {
+
+    const lang = useLanguageStore((s) => s.lang);
+    const t = useLanguageStore((s) => s.t);
+
+      const paragraphs = t("about.paragraphs");
+      const textArray = Array.isArray(paragraphs) ? paragraphs : [];
+
   return (
     <section className='bg-black h-full'>
-      <h2>About</h2>
-      {/* <div>
-        <img src="/car.webp" alt="car" className='w-full h-auto fade-in-up' />
-      </div> */}
       <SceneText/>
+      <article className='text-white p-2 md:p-4'>
+        <h2>{t("about.title")}</h2>
+       {textArray.map((paragraph, i) => (
+        <p
+          key={i}
+          className="text-base"
+          dangerouslySetInnerHTML={{ __html: paragraph }}
+        />
+      ))}
+      </article>
       </section>
   )
 }
