6dd5f3373ffb3087ff04b6ed255b3c4fb460d439
serkan <s8ersis@gmail.com>
2026-08-22 23:57:40 +0300

    chore: update endpoint

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -2,19 +2,19 @@
 const { express } = require("../configs/requiredBasics")
 const router = express.Router()
 
-router.use("/auth", require("./auth"))
+router.use("/api/auth", require("./auth"))
 
-router.use("/users", require("./user"))
+router.use("/api/users", require("./user"))
 
-router.use("/cars", require("./car"))
+router.use("/api/cars", require("./car"))
 
-router.use("/messages", require("./message"));
+router.use("/api/messages", require("./message"));
 
-router.use('/visitors', require('./visitor'))
+router.use('/api/visitors', require('./visitor'))
 
-router.use("/budgets", require("./budget"))
+router.use("/api/budgets", require("./budget"))
 
-router.use("/uploads", require("./upload"))
+router.use("/api/uploads", require("./upload"))
 
 
 module.exports = router
