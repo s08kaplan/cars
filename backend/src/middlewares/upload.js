ae673bb595ea53d7a1d8b31be68cc92be58a222d
serkan <s8ersis@gmail.com>
2026-08-23 03:35:56 +0300

    chore: update upload for users option

--- backend/src/middlewares/upload.js
+++ backend/src/middlewares/upload.js
@@ -1,40 +1,43 @@
-"use strict"
+"use strict";
 
-const multer = require('multer')
-const path = require("path")
-const { sanitizeCarName } = require("../helpers/sanitize")
-const { checkFileExists } = require("../helpers/fileCheck")
+const multer = require("multer");
+const path = require("path");
+const { sanitizeCarName } = require("../helpers/sanitize");
+const { checkFileExists } = require("../helpers/fileCheck");
 
 const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        console.log('Middleware - req.body.brandName:', req.body.brandName);
-         const { type } = req.body;
-        
-        let folder = '';
-        if (type) {
-             const safeCarName = sanitizeCarName(type);
-            folder = path.join('uploads', safeCarName);
-        } 
-     
-        else {
-            folder = path.join('uploads', 'others');
-        }
+  destination: (req, file, cb) => {
+    console.log("Middleware - req.body.brandName:", req.body.brandName);
+    const { type } = req.body;
 
-        console.log('Middleware - folder:', folder)
-        checkFileExists(folder)
-        cb(null, folder);
-    },
-    filename: (req, file, cb) => {
-        cb(null, `${Date.now()}-${file.originalname}`);
-    },
+    let folder = "";
+    if (type) {
+      const safeCarName = sanitizeCarName(type);
+      folder = path.join("uploads", safeCarName);
+    } else if (
+      req.baseUrl.includes("/users") ||
+      req.originalUrl.includes("/users")
+    ) {
+      folder = path.join("uploads", "users");
+    } else {
+      folder = path.join("uploads", "others");
+    }
+
+    console.log("Middleware - folder:", folder);
+    checkFileExists(folder);
+    cb(null, folder);
+  },
+  filename: (req, file, cb) => {
+    cb(null, `${Date.now()}-${file.originalname}`);
+  },
 });
 
 // const upload = multer({ storage })
 
-const upload = multer({ storage })
+const upload = multer({ storage });
 
 // module.exports = upload
 module.exports = {
-    single: upload.single('file'), // For single file uploads
-    multiple: upload.array('files', 10), // For multiple file uploads
-}
+  single: upload.single("file"), // For single file uploads
+  multiple: upload.array("files", 10), // For multiple file uploads
+};
