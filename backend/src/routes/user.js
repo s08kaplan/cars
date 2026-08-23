7d212d079a28aa734ac37e9369a4f96f772ea4cc
serkan <s8ersis@gmail.com>
2026-08-23 03:34:23 +0300

    fix: user controller for uploading image

--- backend/src/routes/user.js
+++ backend/src/routes/user.js
@@ -1,6 +1,6 @@
 "use strict"
 const user = require("../controllers/user")
-
+const upload = require("../middlewares/upload")
 const { express } = require("../configs/requiredBasics")
 const router = express.Router()
 const authorized = require("../middlewares/authorized")
@@ -8,12 +8,12 @@ const authenticatedUser = require("../middlewares/authentication")
 
 router.route("/")
 .get(user.list)
-.post(user.create)
+.post(upload.single, user.create)
 
 router.route("/:userId")
 .get(authenticatedUser,user.read)
-.put(authenticatedUser,user.update)
-.patch(authenticatedUser,user.update)
+.put(authenticatedUser, upload.single, user.update)
+.patch(authenticatedUser, upload.single, user.update)
 .delete(authenticatedUser,user.delete)
 
 module.exports = router
